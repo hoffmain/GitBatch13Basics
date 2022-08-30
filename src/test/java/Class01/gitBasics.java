e9b6583087944b9be46229c34347bb7199e66520
hoffmain <elexia.hoffman@gmail.com>
2022-08-30 15:37:30 -0500

    temperature check

---
+++ src/test/java/Class01/gitBasics.java
@@ -0,0 +1,11 @@
+package Class01;
+
+public class gitBasics {
+    public static void main(String[] args) {
+        /*
+        write down a code that checks the temperature from a website and then
+        prints out that whether it is a beautiful day or not
+         */
+        System.out.println("today is a beautiful day");
+    }
+}
